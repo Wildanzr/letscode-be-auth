fc37c744cb37ca3aab1fec221479b5afd60c5271
wildanzr <graita.sukma@gmail.com>
2022-10-09 14:44:21 +0700

    feat: config app and mongodb

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// Database
+const mongoose = require('mongoose')
+
 // Cors
 const cors = require('cors')
 
 // Init express
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -11,13 +15,19 @@ app.use(express.json())
 // Cors
 app.use(cors())
 
+// Connect to mongodb
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database'))
+
 // Simple route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the application.' })
 })
 
 // Set port, listen for requests
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
