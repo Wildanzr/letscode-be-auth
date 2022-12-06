2dfd7361fc2d06a2b32e209c0712b4524833a0bd
wildanzr <graita.sukma@gmail.com>
2022-12-06 13:27:34 +0700

    fix: endpoint crash

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,8 +5,8 @@ class UserRoutes {
     this.router = express.Router()
     this._userController = userController
 
-    this.router.get('/:username', this._userController.getProfile)
     this.router.post('/profile', this._userController.updateProfile)
+    this.router.get('/profile/:username', this._userController.getProfile)
     this.router.post('/avatar', this._userController.editAvatar)
     this.router.get('/username', this._userController.checkUsernameIsTaken)
     this.router.get('/email', this._userController.checkEmailIsTaken)
