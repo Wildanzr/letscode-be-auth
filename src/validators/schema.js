09e8221d27a447ee82e1a8f90f1e3faed10a34f3
wildanzr <graita.sukma@gmail.com>
2023-04-06 15:15:39 +0700

    fix: schema

--- src/validators/schema.js
+++ src/validators/schema.js
@@ -1,19 +1,19 @@
 const Joi = require('joi')
 
 const registerSchema = Joi.object({
-  username: Joi.string().min(3).max(30).required(),
+  username: Joi.string().min(3).max(50).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).max(30).required(),
+  password: Joi.string().min(8).max(100).required(),
   confirmPassword: Joi.ref('password'),
-  fullName: Joi.string().min(3).max(100).required(),
+  fullName: Joi.string().min(3).max(1000).required(),
   gender: Joi.boolean().truthy('true', 'yes', 1, '1').falsy('false', 'no', 0, '0').required(),
   dateOfBirth: Joi.date().required(),
   role: Joi.number().required().valid(0, 1)
 })
 
 const loginSchema = Joi.object({
-  username: Joi.string().min(3).max(30).required(),
-  password: Joi.string().min(8).max(30).required(),
+  username: Joi.string().min(3).max(50).required(),
+  password: Joi.string().min(8).max(100).required(),
   remember: Joi.boolean().truthy('true', 'yes', 1, '1').falsy('false', 'no', 0, '0').required()
 })
 
@@ -31,28 +31,28 @@ const checkTokenSchema = Joi.object({
 
 const resetPasswordSchema = Joi.object({
   token: Joi.string().required(),
-  password: Joi.string().min(8).max(30).required(),
+  password: Joi.string().min(8).max(100).required(),
   confirmPassword: Joi.ref('password')
 })
 
 const changePasswordSchema = Joi.object({
-  oldPassword: Joi.string().min(8).max(30).required(),
-  newPassword: Joi.string().min(8).max(30).required(),
+  oldPassword: Joi.string().min(8).max(100).required(),
+  newPassword: Joi.string().min(8).max(100).required(),
   confirmNewPassword: Joi.ref('newPassword')
 })
 
 const updateProfileSchema = Joi.object({
-  fullName: Joi.string().min(3).max(200).required(),
-  username: Joi.string().min(3).max(20).required(),
+  fullName: Joi.string().min(3).max(1000).required(),
+  username: Joi.string().min(3).max(50).required(),
   gender: Joi.boolean().truthy('true', 'yes', 1, '1').falsy('false', 'no', 0, '0').required(),
   dateOfBirth: Joi.date().required(),
   address: Joi.string().min(3).max(1000).allow(null),
-  bio: Joi.string().min(3).max(500).allow(null),
+  bio: Joi.string().min(3).max(1000).allow(null),
   phone: Joi.string().min(8).max(20).allow(null)
 })
 
 const checkUsernameSchema = Joi.object({
-  username: Joi.string().min(3).max(20).required()
+  username: Joi.string().min(3).max(50).required()
 })
 
 const checkEmailSchema = Joi.object({
@@ -61,7 +61,7 @@ const checkEmailSchema = Joi.object({
 
 const editPictureSchema = Joi.object({
   mimetype: Joi.string().valid('image/jpeg', 'image/png').required(),
-  size: Joi.number().integer().max(2 * 1024 * 1024).required()
+  size: Joi.number().integer().max(10 * 1024 * 1024).required()
 })
 
 const travelPathSchema = Joi.object({
